67add2a9e7da1c89001b42e6d6b9a19443ea7256
yashdatir <yashdatir@gmail.com>
2021-02-05 17:34:46 +0530

    video recorder

--- src/App.js
+++ src/App.js
@@ -1,39 +1,97 @@
-import "./App.css";
-import React, { useState } from 'react'
- import { Jutsu } from 'react-jutsu'
+import './App.css';
+import React, { useState } from 'react';
+import { Jutsu } from 'react-jutsu';
+
+var recorder, stream;
 
 function App() {
-  const [room, setRoom] = useState('')
-  const [name, setName] = useState('')
-  const [call, setCall] = useState(false)
-  const [password, setPassword] = useState('')
- 
-  const handleClick = event => {
-    event.preventDefault()
-    if (room && name) setCall(true)
-  }
-  return call ? (
-   
-       <Jutsu
-      roomName={room}
-      displayName={name}
-      password={password}
-      onMeetingEnd={() => console.log('Meeting has ended')}
-      loadingComponent={<p>loading ...</p>}
-      errorComponent={<p>Oops, something went wrong</p>} />
-  ) : (
-    <form>
-      <input id='room' type='text' placeholder='Room' value={room} onChange={(e) => setRoom(e.target.value)} />
-      <input id='name' type='text' placeholder='Name' value={name} onChange={(e) => setName(e.target.value)} />
-      <input id='password' type='text' placeholder='Password (optional)' value={password} onChange={(e) => setPassword(e.target.value)} />
-      <button onClick={handleClick} type='submit'>
-        Start / Join
-      </button>
-    </form>
- 
-  
-   )
-}
+  const [src, setSrc] = useState({
+    audio: null,
+    video: null,
+  });
+  const [room, setRoom] = useState('');
+  const [name, setName] = useState('');
+  const [call, setCall] = useState(false);
+  const [password, setPassword] = useState('');
 
+  const handleClick = async (event) => {
+    event.preventDefault();
+    if (room && name) {
+      setCall(true);
+      startRec();
+    }
+  };
+  const startRec = async () => {
+    stream = await navigator.mediaDevices.getDisplayMedia({
+      video: { mediaSource: 'screen' },
+      audio: true,
+    });
+    recorder = new MediaRecorder(stream);
+
+    const chunks = [];
+    recorder.ondataavailable = (e) => chunks.push(e.data);
+    recorder.onstop = (e) => {
+      const completeBlob = new Blob(chunks, { type: chunks[0].type });
+      setSrc({
+        ...src,
+        video: URL.createObjectURL(completeBlob),
+      });
+    };
+    recorder.start();
+  };
+  const stopRec = () => {
+    recorder.stop();
+    stream.getVideoTracks()[0].stop();
+  };
+  return src.video !== null ? (
+    <center>
+      <video src={src.video} width={720} autoPlay controls />
+      {/* <audio src={src.audio} autoPlay controls /> */}
+    </center>
+  ) : call ? (
+    <center>
+      <Jutsu
+        roomName={room}
+        displayName={name}
+        password={password}
+        onMeetingEnd={() => stopRec()}
+        loadingComponent={<p>loading ...</p>}
+        errorComponent={<p>Oops, something went wrong</p>}
+      />
+    </center>
+  ) : (
+    <center>
+      <form>
+        <input
+          id="room"
+          type="text"
+          placeholder="Room"
+          value={room}
+          onChange={(e) => setRoom(e.target.value)}
+        />
+        <br />
+        <input
+          id="name"
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <br />
+        <input
+          id="password"
+          type="text"
+          placeholder="Password (optional)"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <button onClick={handleClick} type="submit">
+          Start / Join
+        </button>
+      </form>
+    </center>
+  );
+}
 
 export default App;
